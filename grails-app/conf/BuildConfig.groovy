f8dc9e1ecc4b5baf6435af053fcee7f93622a66c
Ales Bukovsky <abukovsky@patentproperties.com>
2014-06-16 13:36:59 -0400

    Updated to Grails 2.3.8

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,35 +2,43 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.project.dependency.resolver = "maven"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
+
     inherits("global") {
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    
+    log "warn"
+    
     repositories {
+        grailsPlugins()
         grailsCentral()
         mavenCentral()
     }
+    
     dependencies {
-      //compile 'com.esotericsoftware.kryo:kryo:2.20' 
-      //compile 'org.objenesis:objenesis:1.3'
-      //NOTE: don't include the above libraries. kryo-serializers has all needed dependencies
-      compile 'de.javakaffee:kryo-serializers:0.26'
+        build ('org.kuali.maven.wagons:maven-s3-wagon:1.1.22') {
+            export = false
+            exclude 'slf4j-log4j12'
+        }
+        compile 'de.javakaffee:kryo-serializers:0.26'
     }
 
     plugins {
-    
-        //compile ':spring-security-core:1.2.7.3'
-
-        build(":release:2.0.4") {
+        build (':release:3.0.1') {
             export = false
             exclude 'spock'
         }
-
-        compile(":webxml:1.4.1"){
-          exclude 'spock'
+        compile (":hibernate:3.6.10.16") {
+            export = false  
+        }
+        compile (":webxml:1.4.1") {
+            exclude 'spock'
         }
     }
 }
+
+grails.release.scm.enabled = false
+grails.project.repos.default = "ducktape"
